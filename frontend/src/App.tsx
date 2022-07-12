e74fabc4ab451234b2ebe6503abc37043526dbd8
wilson-developer-git <wilsonprogram@gmail.com>
2022-07-12 10:48:04 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,20 @@
 import NotificationButton from "./components/NotificationButton"
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
 
 
 function App() {
   return (
-      <NotificationButton />
+    <>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
+    </>
   )
 }
 
---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import logo from '../../assets/img/logo.svg'
+
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="DSMeta" />
+                <h1>DS - Metas de vendas</h1>
+                <p>
+                    Desenvolvido por
+                    <a href="https://www.instagram.com/wilson_developer"> @wilson_developer</a>
+                </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
